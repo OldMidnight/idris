81a5daed07f60df5154bf2913267a8daee6cfd2b
OldMidnight <fareedidris20@gmail.com>
2020-08-15 15:29:07 +0100

    Setup Project Structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from setuptools import find_packages, setup
+
+setup(
+        name='idris',
+        version='0.0.1',
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=[],
+    )
